2d14db779b0594c5d4d44f6fae56c21835af9d18
Sim Jun Hong <simjunhong@gmail.com>
2025-09-27 21:44:37 +0800

    Add partners

--- src/Partners.jsx
+++ src/Partners.jsx
@@ -1,5 +1,23 @@
+import allianzLogo from './assets/allianz-logo.png'
+import eqInsuranceLogo from './assets/eq-insurance-logo.png'
+import libertyInsuranceLogo from './assets/liberty-insurance-logo.png'
+
 const Partners = () => (
-  <div>Our Partners</div>
+  <div className="partners">
+    <h2>Our Partners</h2>
+    <div className="partners-logos">
+      <img src={allianzLogo} alt="Allianz Logo" className="partners-logo" />
+      <img src={eqInsuranceLogo} alt="EQ Insurance Logo" className="partners-logo" />
+      <img src={libertyInsuranceLogo} alt="Liberty Insurance Logo" className="partners-logo" />
+      <img src={eqInsuranceLogo} alt="EQ Insurance Logo" className="partners-logo" />
+      <img src={libertyInsuranceLogo} alt="Liberty Insurance Logo" className="partners-logo" />
+      <img src={allianzLogo} alt="Allianz Logo" className="partners-logo" />
+      <img src={libertyInsuranceLogo} alt="Liberty Insurance Logo" className="partners-logo" />
+      <img src={allianzLogo} alt="Allianz Logo" className="partners-logo" />
+      <img src={eqInsuranceLogo} alt="EQ Insurance Logo" className="partners-logo" />
+      <img src={libertyInsuranceLogo} alt="Liberty Insurance Logo" className="partners-logo" />
+    </div>
+  </div>
 )
 
 export default Partners
